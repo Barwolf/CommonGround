deae9d52731b0e3fe44b3f5d4d9187be78016d95
Barwolf <jeremiahlillion@gmail.com>
2026-01-21 23:54:54 -0800

    Moved mobile app to apps/mobile and added Onboarding screen

--- apps/mobile/App.js
+++ apps/mobile/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Onboarding from './src/screens/Onboarding';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>This.... is..... COMMONGROUND!!!</Text>
+      <Onboarding />
       <StatusBar style="auto" />
     </View>
   );
